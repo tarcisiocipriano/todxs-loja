9ba7665785a429ecf487d888e5ea7e7894f07cc3
Tarcisio Cipriano <creativecipriano@gmail.com>
2021-02-24 19:57:06 -0300

    set woocommerce support

--- functions.php
+++ functions.php
@@ -21,5 +21,25 @@ function todxs_loja_config() {
     )
   );
 
+  add_theme_support('woocommerce', array(
+    'thumbnail_image_width' => 255,
+    'single_image_width'    => 255,
+    'product_grid'          => array(
+      'default_rows'    => 10,
+      'min_rows'        => 5,
+      'max_rows'        => 10,
+      'default_columns' => 4,
+      'min_columns'     => 4,
+      'max_columns'     => 4,
+    )
+  ));
+  
+  add_theme_support('wc-product-gallery-zoom');
+  // add_theme_support('wc-product-gallery-lightbox');
+  add_theme_support('wc-product-gallery-slider');
+  
+  if ( ! isset( $content_width ) ) {
+    $content_width = 600;
+  }
 }
 add_action('after_setup_theme', 'todxs_loja_config', 0);
