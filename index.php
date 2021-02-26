6ccff299fadbb0157bba70c96c15a82a948df3af
Tarcisio Cipriano <creativecipriano@gmail.com>
2021-02-25 23:57:19 -0300

    roolback index template to remove row/col classes

--- index.php
+++ index.php
@@ -3,23 +3,21 @@
 <div class="content-area">
   <main>
     <div class="container">
-      <div class="row">
-        <?php
-          if(have_posts()):
-            while(have_posts()): the_post();
-            ?>
-              <article class="col">
-                <h1><?php the_title(); ?></h1>
-                <div><?php the_content(); ?></div>
-              </article>
-            <?php
-            endwhile;
-          else:
-        ?>
-        <p>Nothing to display</p>
-        <?php endif ?>
-      </div> <!-- row -->
-    </div> <!-- container -->
+      <?php
+        if(have_posts()):
+          while(have_posts()): the_post();
+          ?>
+            <article>
+              <h1><?php the_title(); ?></h1>
+              <div><?php the_content(); ?></div>
+            </article>
+          <?php
+          endwhile;
+        else:
+      ?>
+      <p>Nothing to display</p>
+      <?php endif ?>
+    </div>
   </main>
 </div>
 
