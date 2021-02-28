d904097d45da5e0549c1cfdb7991d14350a8b27d
Tarcisio Cipriano <creativecipriano@gmail.com>
2021-02-28 01:16:08 -0300

    improve page layout

--- page.php
+++ page.php
@@ -1,26 +1,18 @@
 <?php get_header(); ?>
 
-<div class="content-area">
-  <main>
-    <div class="container">
-      <div class="row">
-        <?php
-          if(have_posts()):
-            while(have_posts()): the_post();
-            ?>
-              <article class="col">
-                <h1><?php the_title(); ?></h1>
-                <div><?php the_content(); ?></div>
-              </article>
-            <?php
-            endwhile;
-          else:
-        ?>
-        <p>Nothing to display</p>
-        <?php endif ?>
-      </div> <!-- row -->
-    </div> <!-- container -->
-  </main>
-</div>
+<main class="content-area">
+  <div class="container">
+    <div class="row">
+
+      <?php while(have_posts()): the_post(); ?>
+        <article class="col-12">
+          <h1><?php the_title(); ?></h1>
+          <div><?php the_content(); ?></div>
+        </article>
+      <?php endwhile; ?>
+      
+    </div>
+  </div>
+</main>
 
 <?php get_footer(); ?>
