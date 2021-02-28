d39cf180a0f5c2a3c1023846368e894ed228724d
Tarcisio Cipriano <creativecipriano@gmail.com>
2021-02-28 00:10:45 -0300

    refine index template

--- index.php
+++ index.php
@@ -7,9 +7,24 @@
         if(have_posts()):
           while(have_posts()): the_post();
           ?>
-            <article>
-              <h1><?php the_title(); ?></h1>
-              <div><?php the_content(); ?></div>
+            <article <?php post_class(); ?>>
+              <h2>
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+              </h2>
+              <a href="<?php the_permalink(); ?>">
+                <?php
+                  if(has_post_thumbnail()):
+                    the_post_thumbnail('todxs-loja-blog', array('class' => 'img-fluid'));
+                  endif;
+                ?>
+              </a>
+              <div>
+                <p>Publicado por <?php the_author_posts_link(); ?> em <?php echo get_the_date(); ?>
+                  <?php if(has_category()): ?><br />Categorias: <span><?php the_category(' '); ?></span><?php endif; ?>
+                  <?php if(has_tag()): ?><br />Tags: <?php the_tags('', ', '); ?><?php endif; ?>
+                </p>
+              </div>
+              <div><?php the_excerpt(); ?></div>
             </article>
           <?php
           endwhile;
