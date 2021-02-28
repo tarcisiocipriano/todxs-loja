ca04139cd2a243e15acda60558d0dd7a3a6a5504
Tarcisio Cipriano <creativecipriano@gmail.com>
2021-02-28 01:15:49 -0300

    implement single page layout

---
+++ single.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+<main class="content-area">
+  <div class="container">
+    <div class="row">
+    
+      <?php while(have_posts()): the_post(); ?>
+        <article id="post-<?php the_ID(); ?>" class="col-12" <?php post_class(); ?>>
+          <header>
+            <h1><?php the_title(); ?></h1>
+            <div>
+              <p>Publicado por <?php the_author_posts_link(); ?> em <?php echo get_the_date(); ?>
+                <?php if(has_category()): ?><br />Categorias: <span><?php the_category(' '); ?></span><?php endif; ?>
+                <?php if(has_tag()): ?><br />Tags: <?php the_tags('', ', '); ?><?php endif; ?>
+              </p>
+            </div>
+            <?php if(has_post_thumbnail()): the_post_thumbnail('todxs-loja-blog', array('class' => 'img-fluid')); endif; ?>
+          </header>
+          <div><?php the_content(); ?></div>
+        </article>
+      <?php endwhile; ?>
+      
+    </div>
+  </div>
+</main>
+
+<?php get_footer(); ?>
