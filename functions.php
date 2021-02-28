06f952b04528c8e4838e658f7fbeae4fe1e2f80b
Tarcisio Cipriano <creativecipriano@gmail.com>
2021-02-28 16:00:10 -0300

    split register sidebar function

--- functions.php
+++ functions.php
@@ -79,6 +79,9 @@ function todxs_loja_woocommerce_header_add_to_cart_fragment($fragments) {
 	return $fragments;
 }
 
+require_once get_template_directory() . '/inc/register-sidebars.php';
+require_once get_template_directory() . '/inc/wc-show-percentage.php';
+
 // query out of stock products by last
 add_filter('posts_clauses', 'order_by_stock_status');
 function order_by_stock_status($posts_clauses) {
@@ -90,64 +93,4 @@ function order_by_stock_status($posts_clauses) {
     $posts_clauses['where'] = " AND istockstatus.meta_key = '_stock_status' AND istockstatus.meta_value <> '' " . $posts_clauses['where'];
   }
   return $posts_clauses;
-}
-
-require_once get_template_directory() . '/inc/wc-show-percentage.php';
-
-add_action('widgets_init', 'todxs_loja_sidebars');
-function todxs_loja_sidebars() {
-  register_sidebar(array(
-    'name'          => 'Todxs Loja Main Sidebar',
-    'id'            => 'todxs-loja-sidebar-1',
-    'description'   => 'Arraste suas widgets para cá',
-    'before_widget' => '<div id="%1$s" class="widget %2$s widget-wrapper">',
-    'after_widget'  => '</div>',
-    'before_title'  => '<h4 class="widget-title">',
-    'after_title'   => '</h4>'
-  ));
-  register_sidebar(array(
-    'name'          => 'Todxs Loja Shop Sidebar',
-    'id'            => 'todxs-loja-sidebar-shop',
-    'description'   => 'Arraste suas widgets da loja para cá',
-    'before_widget' => '<div id="%1$s" class="widget %2$s widget-wrapper">',
-    'after_widget'  => '</div>',
-    'before_title'  => '<h4 class="widget-title">',
-    'after_title'   => '</h4>'
-  ));
-  register_sidebar(array(
-    'name'          => 'Todxs Loja Header Sidebar',
-    'id'            => 'todxs-loja-sidebar-header',
-    'description'   => 'Arraste suas widgets para cá',
-    'before_widget' => '<div id="%1$s" class="widget %2$s widget-wrapper widget-header">',
-    'after_widget'  => '</div>',
-    'before_title'  => '<h4 class="widget-title">',
-    'after_title'   => '</h4>'
-  ));
-  register_sidebar(array(
-    'name'          => 'Todxs Loja Footer Sidebar 1',
-    'id'            => 'todxs-loja-sidebar-footer-1',
-    'description'   => 'Arraste suas widgets para cá',
-    'before_widget' => '<div id="%1$s" class="widget %2$s widget-wrapper">',
-    'after_widget'  => '</div>',
-    'before_title'  => '<h4 class="widget-title">',
-    'after_title'   => '</h4>'
-  ));
-  register_sidebar(array(
-    'name'          => 'Todxs Loja Footer Sidebar 2',
-    'id'            => 'todxs-loja-sidebar-footer-2',
-    'description'   => 'Arraste suas widgets para cá',
-    'before_widget' => '<div id="%1$s" class="widget %2$s widget-wrapper">',
-    'after_widget'  => '</div>',
-    'before_title'  => '<h4 class="widget-title">',
-    'after_title'   => '</h4>'
-  ));
-  register_sidebar(array(
-    'name'          => 'Todxs Loja Footer Sidebar 3',
-    'id'            => 'todxs-loja-sidebar-footer-3',
-    'description'   => 'Arraste suas widgets para cá',
-    'before_widget' => '<div id="%1$s" class="widget %2$s widget-wrapper">',
-    'after_widget'  => '</div>',
-    'before_title'  => '<h4 class="widget-title">',
-    'after_title'   => '</h4>'
-  ));
 }
---
+++ inc/register-sidebars.php
@@ -0,0 +1,59 @@
+<?php
+
+add_action('widgets_init', 'todxs_loja_sidebars');
+function todxs_loja_sidebars() {
+  register_sidebar(array(
+    'name'          => 'Todxs Loja Main Sidebar',
+    'id'            => 'todxs-loja-sidebar-1',
+    'description'   => 'Arraste suas widgets para cá',
+    'before_widget' => '<div id="%1$s" class="widget %2$s widget-wrapper">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h4 class="widget-title">',
+    'after_title'   => '</h4>'
+  ));
+  register_sidebar(array(
+    'name'          => 'Todxs Loja Shop Sidebar',
+    'id'            => 'todxs-loja-sidebar-shop',
+    'description'   => 'Arraste suas widgets da loja para cá',
+    'before_widget' => '<div id="%1$s" class="widget %2$s widget-wrapper">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h4 class="widget-title">',
+    'after_title'   => '</h4>'
+  ));
+  register_sidebar(array(
+    'name'          => 'Todxs Loja Header Sidebar',
+    'id'            => 'todxs-loja-sidebar-header',
+    'description'   => 'Arraste suas widgets para cá',
+    'before_widget' => '<div id="%1$s" class="widget %2$s widget-wrapper widget-header">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h4 class="widget-title">',
+    'after_title'   => '</h4>'
+  ));
+  register_sidebar(array(
+    'name'          => 'Todxs Loja Footer Sidebar 1',
+    'id'            => 'todxs-loja-sidebar-footer-1',
+    'description'   => 'Arraste suas widgets para cá',
+    'before_widget' => '<div id="%1$s" class="widget %2$s widget-wrapper">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h4 class="widget-title">',
+    'after_title'   => '</h4>'
+  ));
+  register_sidebar(array(
+    'name'          => 'Todxs Loja Footer Sidebar 2',
+    'id'            => 'todxs-loja-sidebar-footer-2',
+    'description'   => 'Arraste suas widgets para cá',
+    'before_widget' => '<div id="%1$s" class="widget %2$s widget-wrapper">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h4 class="widget-title">',
+    'after_title'   => '</h4>'
+  ));
+  register_sidebar(array(
+    'name'          => 'Todxs Loja Footer Sidebar 3',
+    'id'            => 'todxs-loja-sidebar-footer-3',
+    'description'   => 'Arraste suas widgets para cá',
+    'before_widget' => '<div id="%1$s" class="widget %2$s widget-wrapper">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h4 class="widget-title">',
+    'after_title'   => '</h4>'
+  ));
+}
