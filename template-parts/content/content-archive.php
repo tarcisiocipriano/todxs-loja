43fd02a9f87c95e6722e6cbf1d7b15bc723be7db
Tarcisio Cipriano <creativecipriano@gmail.com>
2021-02-28 05:28:36 -0300

    remove unnecessary col classes

--- template-parts/content/content-archive.php
+++ template-parts/content/content-archive.php
@@ -1,4 +1,4 @@
-<article class="col-12" <?php post_class(); ?>>
+<article <?php post_class(); ?>>
   <h2>
     <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
   </h2>
--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -1,4 +1,4 @@
-<article id="post-<?php the_ID(); ?>" class="col-12" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <header>
     <h1><?php the_title(); ?></h1>
     <div>
