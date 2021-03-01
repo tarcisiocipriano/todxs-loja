a46cfedc6bf5930d31ff111d9b65a0511d7624dc
Tarcisio Cipriano <creativecipriano@gmail.com>
2021-03-01 01:48:45 -0300

    query for amount of products

--- functions.php
+++ functions.php
@@ -87,3 +87,12 @@ function order_by_stock_status($posts_clauses) {
   }
   return $posts_clauses;
 }
+
+// cshange number of products that are displayed per page (shop page)
+add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page', 20 );
+function new_loop_shop_per_page( $cols ) {
+  // $cols contains the current number of products per page based on the value stored on Options –> Reading
+  // Return the number of products you wanna show per page.
+  $cols = 16;
+  return $cols;
+}
